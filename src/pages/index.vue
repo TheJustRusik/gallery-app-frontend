ea8df7c0ce38ee9f7c1ef089a4a24634d0967a49
TheJustRusik <7thejustrusik7@gmail.com>
2024-02-25 16:09:40 +0600

    ?

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,8 +4,14 @@
       <div>Welcome to</div>
       <h1>Gallery</h1>
       <div>
-        <img v-for="url in data" :src="'http://localhost:8080/' + url" alt="">
+        <img v-for="url in data" :src="'http://localhost:8080/images' + url" alt="">
       </div>
+      <h2>Uppload your image!</h2>
+      <div style="padding-bottom: 45px;">
+        <v-file-input v-model="file" clearable label="File input" variant="underlined"></v-file-input>
+        <v-btn @click="uploadFile">Upload</v-btn>
+      </div>
+      
       <v-btn
         color="pink"
         dark
@@ -27,9 +33,21 @@
   import axios from 'axios';
 
   let data = ref([])
+  let file = ref()
+
+  function uploadFile(){
+    const formData = new FormData();
+    formData.append('image', file.value);
+    axios.post('http://localhost:8080/images', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+      
+  }
 
   async function load_galery(){
-    const response = await axios.get('http://localhost:8080/api/content_images');
+    const response = await axios.get('http://localhost:8080/');
     data.value = response.data
   }
   load_galery()
